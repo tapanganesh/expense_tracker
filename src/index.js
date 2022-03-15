3e91eecea0facaa1ece3de2749991b083468a718
TapanGanesh <tapanganesh.naram@gmail.com>
2022-03-15 17:17:11 +0530

    rotuing

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {HashRouter} from 'react-router-dom'
+import { HashRouter } from "react-router-dom";
 import { Provider } from 'react-redux'
 import { store } from './Store'
 ReactDOM.render(
